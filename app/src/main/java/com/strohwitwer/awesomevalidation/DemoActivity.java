a15d0914e4dc482611ba06cf2d78ede23456664d
Jing Li <thyrlian@gmail.com>
2014-12-15 18:00:11 +0100

    Create project

---
+++ app/src/main/java/com/strohwitwer/awesomevalidation/DemoActivity.java
@@ -0,0 +1,15 @@
+package com.strohwitwer.awesomevalidation;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class DemoActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+    }
+
+}
