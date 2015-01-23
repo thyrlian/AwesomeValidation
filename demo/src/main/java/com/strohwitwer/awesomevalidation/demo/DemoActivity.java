603dd4dd078cbffb6542e80bd68a7ab16f7d2282
Jing Li <thyrlian@gmail.com>
2015-01-23 18:06:07 +0100

    Add navigation drawer to toggle different validation styles for the demo.

--- demo/src/main/java/com/strohwitwer/awesomevalidation/demo/DemoActivity.java
+++ demo/src/main/java/com/strohwitwer/awesomevalidation/demo/DemoActivity.java
@@ -1,32 +1,133 @@
 package com.strohwitwer.awesomevalidation.demo;
 
+import android.app.Activity;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.strohwitwer.awesomevalidation.AwesomeValidation;
 import com.strohwitwer.awesomevalidation.ValidationStyle;
 
+import static com.strohwitwer.awesomevalidation.ValidationStyle.BASIC;
+import static com.strohwitwer.awesomevalidation.ValidationStyle.COLORATION;
+
 public class DemoActivity extends ActionBarActivity {
+
+    private String[] mStyles;
+    private String mStyle;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private DrawerItemClickListener mDrawerItemClickListener = new DrawerItemClickListener();
+    private int mPosition = 0;
+    private AwesomeValidation mAwesomeValidation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
+        mStyles = getResources().getStringArray(R.array.styles);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mStyles));
+        mDrawerList.setOnItemClickListener(mDrawerItemClickListener);
+        mDrawerItemClickListener.selectItem(mPosition);
+    }
 
-        Button btnDone = (Button) findViewById(R.id.btn_done);
-        final AwesomeValidation awesomeValidation = new AwesomeValidation(ValidationStyle.COLORATION);
-        awesomeValidation.setColor(Color.YELLOW);
-        awesomeValidation.addValidation(this, R.id.edt_userid, "[a-zA-Z0-9_-]+", R.string.err_userid);
-        awesomeValidation.addValidation(this, R.id.edt_name, "[a-zA-Z\\s]+", R.string.err_name);
-        awesomeValidation.addValidation(this, R.id.edt_tel, "\\d+", R.string.err_tel);
-        awesomeValidation.addValidation(this, R.id.edt_zipcode, "\\d+", R.string.err_zipcode);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mDrawerLayout.isDrawerOpen(Gravity.START|Gravity.LEFT)){
+            mDrawerLayout.closeDrawers();
+            return;
+        }
+        if (mPosition > 0) {
+            mDrawerItemClickListener.selectItem(0);
+            return;
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+
+        private void selectItem(int position) {
+            mDrawerList.setItemChecked(position, true);
+            mPosition = position;
+            mStyle = mStyles[mPosition];
+            setTitle(mStyle);
+            mDrawerLayout.closeDrawer(mDrawerList);
+
+            initValidation(mStyle);
+            addValidation(DemoActivity.this);
+        }
+    }
+
+    private void initValidation(String style) {
+        switch (ValidationStyle.valueOf(style)) {
+            case BASIC:
+                mAwesomeValidation = new AwesomeValidation(BASIC);
+                break;
+            case COLORATION:
+                mAwesomeValidation = new AwesomeValidation(COLORATION);
+                mAwesomeValidation.setColor(Color.YELLOW);
+                break;
+        }
+    }
+
+    private void addValidation(final Activity activity) {
+        mAwesomeValidation.addValidation(activity, R.id.edt_userid, "[a-zA-Z0-9_-]+", R.string.err_userid);
+        mAwesomeValidation.addValidation(activity, R.id.edt_name, "[a-zA-Z\\s]+", R.string.err_name);
+        mAwesomeValidation.addValidation(activity, R.id.edt_tel, "\\d+", R.string.err_tel);
+        mAwesomeValidation.addValidation(activity, R.id.edt_zipcode, "\\d+", R.string.err_zipcode);
+
+        Button btnDone = (Button) activity.findViewById(R.id.btn_done);
         btnDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                awesomeValidation.validate();
+                mAwesomeValidation.validate();
             }
         });
     }
