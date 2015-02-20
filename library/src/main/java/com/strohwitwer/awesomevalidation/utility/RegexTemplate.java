b5883ac1196e2db8b17f390a095826d3d20f511f
Jing Li <thyrlian@gmail.com>
2015-02-20 22:20:34 +0100

    Update telephone regex template.

--- library/src/main/java/com/strohwitwer/awesomevalidation/utility/RegexTemplate.java
+++ library/src/main/java/com/strohwitwer/awesomevalidation/utility/RegexTemplate.java
@@ -2,7 +2,7 @@ package com.strohwitwer.awesomevalidation.utility;
 
 public class RegexTemplate {
 
-    public static final String TELEPHONE = "^\\+?[0-9()-]*";
+    public static final String TELEPHONE = "(^\\+)?[0-9()-]*";
 
     private RegexTemplate() {
         throw new AssertionError();
