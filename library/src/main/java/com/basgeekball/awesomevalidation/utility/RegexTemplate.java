edf805978fd16917fd077f8ea959bd76e22f9d38
Jing Li <jingli@ebay.com>
2015-04-02 09:15:25 +0200

    Add a regex constant for checking empty field.

--- library/src/main/java/com/basgeekball/awesomevalidation/utility/RegexTemplate.java
+++ library/src/main/java/com/basgeekball/awesomevalidation/utility/RegexTemplate.java
@@ -2,6 +2,7 @@ package com.basgeekball.awesomevalidation.utility;
 
 public class RegexTemplate {
 
+    public static final String NOT_EMPTY = "^(?=\\s*\\S).*$";
     public static final String TELEPHONE = "(^\\+)?[0-9()-]*";
 
     private RegexTemplate() {
