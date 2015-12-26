0ae62cece555a3a76e58f81d231166ef524b9ebc
Jing Li <jingli@ebay.com>
2015-12-26 22:25:27 +0100

    Add unit tests for ValidationStyle

---
+++ library/src/test/java/com/basgeekball/awesomevalidation/ValidationStyleTest.java
@@ -0,0 +1,29 @@
+package com.basgeekball.awesomevalidation;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class ValidationStyleTest extends TestCase {
+
+    public void testFromValueToBasic() {
+        assertEquals(ValidationStyle.BASIC, ValidationStyle.fromValue(0));
+    }
+
+    public void testFromValueToColoration() {
+        assertEquals(ValidationStyle.COLORATION, ValidationStyle.fromValue(1));
+    }
+
+    public void testFromValueToUnderlabel() {
+        assertEquals(ValidationStyle.UNDERLABEL, ValidationStyle.fromValue(2));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFromValueThrowException() {
+        ValidationStyle.fromValue(3);
+    }
+
+}
