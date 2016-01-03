63fe2a8cde51e0fe0b5817673d0a7890a9110116
Jing Li <jingli@ebay.com>
2016-01-03 04:02:03 +0100

    Add tests for AwesomeValidation#addValidation

--- library/src/test/java/com/basgeekball/awesomevalidation/AwesomeValidationTest.java
+++ library/src/test/java/com/basgeekball/awesomevalidation/AwesomeValidationTest.java
@@ -1,10 +1,14 @@
 package com.basgeekball.awesomevalidation;
 
+import android.app.Activity;
 import android.content.Context;
+import android.widget.EditText;
 
+import com.basgeekball.awesomevalidation.model.NumericRange;
 import com.basgeekball.awesomevalidation.validators.BasicValidator;
 import com.basgeekball.awesomevalidation.validators.ColorationValidator;
 import com.basgeekball.awesomevalidation.validators.UnderlabelValidator;
+import com.google.common.collect.Range;
 
 import junit.framework.TestCase;
 
@@ -15,13 +19,17 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
+import java.util.regex.Pattern;
+
+import static org.mockito.Mockito.RETURNS_MOCKS;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(AwesomeValidation.class)
+@PrepareForTest({AwesomeValidation.class, Range.class})
 public class AwesomeValidationTest extends TestCase {
 
     public void testAwesomeValidationConstructBasicStyle() {
@@ -71,6 +79,42 @@ public class AwesomeValidationTest extends TestCase {
         MemberModifier.field(AwesomeValidation.class, "mValidator").set(spiedAwesomeValidation, spiedBasicValidator);
         int color = 256;
         doThrow(UnsupportedOperationException.class).when(spiedAwesomeValidation).setColor(color);
+    }
+
+    public void testAddValidation() throws Exception {
+        BasicValidator spiedValidator = spy(BasicValidator.class);
+        AwesomeValidation spiedAwesomeValidation = spy(new AwesomeValidation(ValidationStyle.BASIC));
+        MemberModifier.field(AwesomeValidation.class, "mValidator").set(spiedAwesomeValidation, spiedValidator);
+
+        Activity mockedActivity = mock(Activity.class, RETURNS_MOCKS);
+        EditText mockedEditText = mock(EditText.class, RETURNS_MOCKS);
+        Pattern mockedPattern = PowerMockito.mock(Pattern.class, RETURNS_MOCKS);
+        Range mockedRange = PowerMockito.mock(Range.class, RETURNS_MOCKS);
+        String mockedRegex = PowerMockito.mock(String.class);
+        String mockedErrMsg = PowerMockito.mock(String.class);
+        NumericRange mockedNumericRange = mock(NumericRange.class);
+        int viewId = 65536;
+        int errMsgId = 32768;
+        when(mockedActivity.findViewById(viewId)).thenReturn(mockedEditText);
+        PowerMockito.whenNew(NumericRange.class).withArguments(mockedRange).thenReturn(mockedNumericRange);
+
+        spiedAwesomeValidation.addValidation(mockedEditText, mockedRegex, mockedErrMsg);
+        PowerMockito.verifyPrivate(spiedValidator, times(1)).invoke("set", mockedEditText, mockedRegex, mockedErrMsg);
+
+        spiedAwesomeValidation.addValidation(mockedActivity, viewId, mockedRegex, errMsgId);
+        PowerMockito.verifyPrivate(spiedValidator, times(1)).invoke("set", mockedActivity, viewId, mockedRegex, errMsgId);
+
+        spiedAwesomeValidation.addValidation(mockedEditText, mockedPattern, mockedErrMsg);
+        PowerMockito.verifyPrivate(spiedValidator, times(1)).invoke("set", mockedEditText, mockedPattern, mockedErrMsg);
+
+        spiedAwesomeValidation.addValidation(mockedActivity, viewId, mockedPattern, errMsgId);
+        PowerMockito.verifyPrivate(spiedValidator, times(1)).invoke("set", mockedActivity, viewId, mockedPattern, errMsgId);
+
+        spiedAwesomeValidation.addValidation(mockedEditText, mockedRange, mockedErrMsg);
+        PowerMockito.verifyPrivate(spiedValidator, times(1)).invoke("set", mockedEditText, new NumericRange(mockedRange), mockedErrMsg);
+
+        spiedAwesomeValidation.addValidation(mockedActivity, viewId, mockedRange, errMsgId);
+        PowerMockito.verifyPrivate(spiedValidator, times(1)).invoke("set", mockedActivity, viewId, new NumericRange(mockedRange), errMsgId);
     }
 
     public void testValidate() throws Exception {
