5166325b1fcaa6d0a62d3e0bb4c344c3babc80cb
Amin Mahboubi <aminmahboobi@gmail.com>
2018-01-28 20:16:43 +0100

    Abstract class changed to Interface

--- library/src/main/java/com/basgeekball/awesomevalidation/helper/CustomValidation.java
+++ library/src/main/java/com/basgeekball/awesomevalidation/helper/CustomValidation.java
@@ -4,6 +4,6 @@ package com.basgeekball.awesomevalidation.helper;
  * Created by amin on 1/28/18.
  */
 
-public abstract class CustomValidation {
-    public abstract boolean compare(String input);
+public interface CustomValidation {
+     boolean compare(String input);
 }
